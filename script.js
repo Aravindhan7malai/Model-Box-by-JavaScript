930d6122534d123c6bffc9c05f199badd8dcfbcc
Aravindhan Elumalai <159620206+Aravindhan7malai@users.noreply.github.com>
2024-06-03 12:30:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const button = document.getElementsByClassName('btn')[0];
+const model = document.getElementsByClassName('model_box')[0];
+const button_2 = document.getElementsByClassName('btn_2')[0];
+button.addEventListener('click', function () {
+    model.classList.remove('hide');
+
+})
+button_2.addEventListener('click', function () {
+    model.classList.add('hide');
+})
