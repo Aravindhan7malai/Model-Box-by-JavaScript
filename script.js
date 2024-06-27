40062d7c6a5504e1fa8e26c66bd53a9163fdd1cb
Aravindhan Elumalai <159620206+Aravindhan7malai@users.noreply.github.com>
2024-06-28 01:13:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.querySelector('.btn');
+    const modelBox = document.querySelector('.model_box');
+    const buttonClose = document.querySelector('.btn_2');
+
+    button.addEventListener('click', function() {
+        modelBox.classList.remove('hide');
+    });
+
+    buttonClose.addEventListener('click', function() {
+        modelBox.classList.add('hide');
+    });
+
+    document.addEventListener('keydown', function(event) {
+        if (event.key === 'Escape') {
+            modelBox.classList.add('hide');
+        }
+    });
+});
+
+
+
+
+
 // const button = document.getElementsByClassName('btn')[0];
 // const model = document.getElementsByClassName('model_box')[0];
 // const button_2 = document.getElementsByClassName('btn_2')[0];
@@ -15,23 +39,23 @@
 // })
 
 
-document.addEventListener('DOMContentLoaded', function() {
-    const button = document.querySelector('.btn');
-    const model = document.querySelector('.model_box');
-    const buttonClose = document.querySelector('.btn_2');
+// document.addEventListener('DOMContentLoaded', function() {
+//     const button = document.querySelector('.btn');
+//     const model = document.querySelector('.model_box');
+//     const buttonClose = document.querySelector('.btn_2');
 
-    button.addEventListener('click', function () {
-        model.classList.remove('hide');
-    });
+//     button.addEventListener('click', function () {
+//         model.classList.remove('hide');
+//     });
 
-    buttonClose.addEventListener('click', function () {
-        model.classList.add('hide');
-    });
+//     buttonClose.addEventListener('click', function () {
+//         model.classList.add('hide');
+//     });
 
-    document.addEventListener('keydown', function(event) {
-        if (event.key === 'Escape') {
-            model.classList.add('hide');
-        }
-    });
+//     document.addEventListener('keydown', function(event) {
+//         if (event.key === 'Escape') {
+//             model.classList.add('hide');
+//         }
+//     });
 });
 
